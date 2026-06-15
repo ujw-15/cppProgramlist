8c946da3eb34a10c4bac243f94e6b1191a837387
ujw-15 <umsunggyu@gmail.com>
2026-06-15 19:44:12 +0900

    (2026,6,15) 원형 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -38,13 +38,101 @@ public:
 		}
 		else
 		{
-			head->previous = newNode; // <-
-			newNode->next = head; // ->
+			head->previous = newNode; 
+			newNode->next = head; 
 
-			head = newNode; // 이동
+			head = newNode; 
 		}
 		size++;
 	}
+
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked list is empty" << endl;
+		}
+		else
+		{
+			Node* deleteNode = head;
+
+			if (head == tail)
+			{
+				head = nullptr;
+				tail = nullptr;
+			}
+			else
+			{
+				deleteNode->next->previous = nullptr;
+				head = head->next;
+			}
+			
+			delete deleteNode;
+
+			size--;
+		}
+	}
+
+	void push_back(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = nullptr;
+		newNode->previous = nullptr;
+
+		if (tail == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			newNode->previous = tail;
+			tail = newNode;
+		}
+		size++;
+	}
+
+	void pop_back()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked list is empty" << endl;
+		}
+		else
+		{
+			Node* deleteNode = tail;
+
+			if (head == tail)
+			{
+				head = nullptr;
+				tail = nullptr;
+			}
+			else
+			{
+				tail->previous->next = nullptr;
+				tail = tail->previous;
+			}
+			delete deleteNode;
+			size--;
+
+		}
+	}
+
+	const bool& empty()
+	{
+		return head == nullptr;
+	}
+
+	~List()
+	{
+		while (head != nullptr)
+		{
+			pop_front();
+		}
+	}
+
 };
 
 int main()
@@ -52,6 +140,13 @@ int main()
 	List<int>list;
 	list.push_front(10);
 	list.push_front(20);
+	list.push_back(30);
+	list.push_back(40);
+	list.pop_front();
+	list.pop_front();
+	list.pop_front();
+	list.pop_back();
+	list.pop_back();
 	return 0;
 }
 
