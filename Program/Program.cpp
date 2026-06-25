dd23e4adfa48c89c7655fe1fdba6f8ababbbe871
ujw-15 <umsunggyu@gmail.com>
2026-06-25 19:48:27 +0900

    (2026,6,25)  해쉬 테이블

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -36,14 +36,109 @@ public:
 
 		container = temporary;
 	}
+
+	void push(T data)
+	{
+		if (capacity <= 0)
+		{
+			resize(1);
+		}
+		else if(index >= capacity)
+		{
+			resize(capacity * 2);
+		}
+		container[index++] = data;
+
+		int child = index - 1;
+		int parent = (child - 1) / 2;
+		
+		while (child > 0)
+		{
+			if (container[parent] < container[child])
+			{
+				swap(container[parent], container[child]);
+			}
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+	}
+
+	void pop()
+	{
+		if (index <= 0)
+		{
+			cout << "Priority queue is empty" << endl;
+		}
+		else
+		{
+			container[0] = container[--index];
+
+			container[index] = NULL;
+
+			int parent = 0;
+			int child = parent * 2 + 1;
+
+			while (child < index)
+			{
+				child = parent * 2 + 1;
+
+				if (container[child] < container[child] + 1)
+				{
+					child++;
+				}
+
+				if (container[child] < container[parent])
+				{
+					break;
+				}
+				else
+				{
+					std::swap(container[parent], container[child]);
+					parent = child;
+				}
+			}
+		}
+	}
+
+	~PriorityQueue()
+	{
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+	}
+
+	const T& top()
+	{
+		return container[0];
+	}
+
+	const int& size()
+	{
+		return index;
+	}
+	
+	const bool& empty()
+	{
+		return index <= 0;
+	}
+
+
 };
 
 int main()
 {
 	PriorityQueue<int>pri_queue;
-	pri_queue.resize(1);
-	pri_queue.resize(2);
-	pri_queue.resize(4);
+	pri_queue.push(10);
+	pri_queue.push(20);
+	pri_queue.push(30);
+	
+
+	while (pri_queue.empty() == false)
+	{
+		cout << pri_queue.top() << endl;
+		pri_queue.pop();
+	}
 
 	return 0;
 }
