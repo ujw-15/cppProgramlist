1e95c7c7702b58e82de2a40a8d17167997ab9bde
ujw-15 <umsunggyu@gmail.com>
2026-06-30 04:21:53 +0900

    (2026,6,29) 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -38,7 +38,7 @@ public:
 	}
 
 	template<typename KEY>
-	unsigned int hasf_function(KEY key)
+	unsigned int hash_function(KEY key)
 	{
 		return (unsigned int)key % capacity;
 	}
@@ -47,7 +47,7 @@ public:
 	unsigned int hash_function(const char* key)
 	{
 		unsigned int sum = 0;
-		for (int i = 0; i * key != '\0'; i++)
+		for (int i = 0; *key != '\0'; i++)
 		{
 			sum += key[i];
 			key = key + 1;
@@ -55,14 +55,62 @@ public:
 		return sum % capacity;
 	}
 
+	void insert(KEY key, VALUE value)
+	{
+		int hashIndex = hash_function(key);
+
+		Node* newNode = new Node;
+
+		newNode->k = key; 
+		newNode->v = value;
+
+		newNode->next = nullptr;
+
+		if (bucket[hashIndex].count == 0)
+		{
+			bucket[hashIndex].head = newNode;
+		}
+		else
+		{
+			newNode->next = bucket[hashIndex].head;
+
+			bucket[hashIndex].head = newNode;
+		}
+
+		bucket[hashIndex].count++;
+
+		size++;
+	}
+	~HashTable()
+	{
+		for (int i = 0; i < capacity; i++)
+		{
+			Node* deleteNode = bucket[i].head;
+			Node* nextNode = bucket[i].head;
+
+			if (bucket[i].head == nullptr)
+			{
+				continue;
+			}
+			else
+			{
+				while (nextNode != nullptr)
+				{
+					nextNode = deleteNode->next;
+					delete deleteNode;
+				}
+			}
+		}
+		delete[] bucket;
+	}
+
 };
 
 int main()
 {
-	HashTable<int, int> hashTable;
+	HashTable<const char *, int> hashTable;
 
-	cout << hashTable.hasf_function(3129) << endl;
-	cout << hashTable.hasf_function(-3) << endl;
+	hashTable.insert("abc", 12);
 
 	return 0;
 }
